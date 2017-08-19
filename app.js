30bd4745f0a5f64a70ab71dcb2dccba2cd096e41
Duy Ngo <ntduy96@gmail.com>
2017-08-19 15:12:05 +0700

    add post and get methods
    add post method to receive tweet from client
    and get method for get all tweets saved

--- app.js
+++ app.js
@@ -1,8 +1,37 @@
-var express = require('express')
+var express = require('express'),
+		bodyParser =  require('body-parser')
 
+var app = express()
 
-app.get('', (req, res) => {
-	res.send('Welcome to Node Twitter')
-}).listen(8000, () => {
+app.use(bodyParser.urlencoded({ extended: false}))
+.use(bodyParser.json())
+
+app.listen(8000, () => {
 	console.log('App running on port 8000')
+})
+
+var tweets = []
+
+app.get('/', (req, res) => {
+	res.send('Welcome to Node Twitter')
+})
+
+app.post('/send', (req, res) => {
+	if (req.body && req.body.tweet) {
+		tweets.push(req.body.tweet)
+		res.send({
+			status: 'ok',
+			message: 'Tweet received'
+		})
+	} else {
+		//no tweet?
+		res.send({
+			status: 'nok',
+			message: 'No tweet received'
+		})
+	}
+})
+
+app.get('/tweets', (req, res) => {
+	res.send(tweets)
 })
